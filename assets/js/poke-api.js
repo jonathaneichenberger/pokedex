e5f7627dfec0a94d06ab20bab0c86780f2b74c0a
Jonathan Eichenberger <djohnni@hotmail.com>
2025-01-02 12:35:46 -0300

    Adicionado ao CSS as cores por tipo de pokemon

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -23,7 +23,7 @@ pokeApi.getPokemonDetail = (pokemon) => {
 }
 
 // offset e limit recebidos como paramentros estão com valores para serem valores default, caso não seja passado nenhum parametro.
-pokeApi.getPokemons = (offset = 0, limit = 10) => {
+pokeApi.getPokemons = (offset = 0, limit = 20) => {
 
     const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 
